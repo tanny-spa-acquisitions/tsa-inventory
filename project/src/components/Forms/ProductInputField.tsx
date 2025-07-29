c1dc31223b05ec1cfe83dcfdd619e78dbf8ba421
JosephGoff <joeygoff13@gmail.com>
2025-07-29 12:00:22 -0400

    4

--- project/src/components/Forms/ProductInputField.tsx
+++ project/src/components/Forms/ProductInputField.tsx
@@ -91,24 +91,41 @@ const ProductInputField = <T extends FieldValues>({
           rows={rows}
         />
       ) : inputType === "dropdown" ? (
-        <select
-          {...register(name)}
-          className="input rounded-[7px] w-[100%] mt-[6px] px-[6px] py-[4px]"
-          style={{
-            border: `0.5px solid ${
-              currentUser.theme === "light"
-                ? appTheme[currentUser.theme].text_3
-                : appTheme[currentUser.theme].text_4
-            }`,
-          }}
-        >
-          {options &&
-            options.map((option: string) => (
-              <option key={option} value={option}>
-                {option}
-              </option>
-            ))}
-        </select>
+<div className="relative">
+  <select
+    {...register(name)}
+    className="input rounded-[7px] w-full px-3 py-[4px] custom-select pr-8"
+    style={{
+      appearance: "none",
+      WebkitAppearance: "none",
+      backgroundColor: "transparent",
+      border: `0.5px solid ${
+        currentUser.theme === "light"
+          ? appTheme[currentUser.theme].text_3
+          : appTheme[currentUser.theme].text_4
+      }`,
+      color: appTheme[currentUser.theme].text_1,
+    }}
+  >
+    {options &&
+      options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+  </select>
+  <div className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="14"
+      fill={appTheme[currentUser.theme].text_1}
+      viewBox="0 0 20 20"
+    >
+      <path d="M5.23 7.21a.75.75 0 011.06.02L10 11.06l3.71-3.83a.75.75 0 111.08 1.04l-4.25 4.39a.75.75 0 01-1.08 0L5.21 8.27a.75.75 0 01.02-1.06z" />
+    </svg>
+  </div>
+</div>
       ) : inputType === "date" ? (
         <div className="flex flex-row gap-[10px] mt-[6px]">
           <div
